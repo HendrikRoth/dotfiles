5f5ea8e695b8b5753f0539e236167ce7a7c5ddf4
Hendrik Roth <hi@hendrikroth.com>
2022-09-21 01:19:09 +0200

    vertical terminal

--- .config/nvim/lua/user/terminal.lua
+++ .config/nvim/lua/user/terminal.lua
@@ -1,5 +1,10 @@
 require("toggleterm").setup {
+  auto_scroll = true,
+  autochdir = true,
+  direction = 'vertical',
   hide_numbers = true,
+  persist_mode = true,
+  persist_size = true,
   start_in_insert = true,
   float_opts = {
     border = "curved",
